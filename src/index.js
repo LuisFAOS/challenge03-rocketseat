6daa01890740f3c45beabc1515f5f70b5a281815
LuisFAOS <luisfaos.web@gmail.com>
2021-11-14 20:07:22 -0300

    errors fixed and all tests passed

--- src/index.js
+++ src/index.js
@@ -23,6 +23,8 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
+   repositories.push(repository)
+
   return response.json(repository);
 });
 
@@ -30,13 +32,14 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  let repository = { ...repositories[repositoryIndex], ...updatedRepository };
+   repository.likes = repositories[repositoryIndex].likes
 
   repositories[repositoryIndex] = repository;
 
@@ -48,13 +51,13 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
   repositories.splice(repositoryIndex, 1);
 
-  return response.status(204).send();
+  return response.status(204).json({success: 'repo has been deleted!'});
 });
 
 app.post("/repositories/:id/like", (request, response) => {
@@ -68,7 +71,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.status(200).json({likes});
 });
 
 module.exports = app;
